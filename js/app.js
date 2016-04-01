38c3489afa5b05c754c48d6ff12e55050793b967
D Shaffer <404shaff@gmail.com>
2016-04-01 14:32:45 -0400

    moves array

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
 
 var $gameCell = $('.cell');
+var moves = Array(9).join(".").split(".");
+var count = 0;
 
   $($gameCell).one('click', function() {
-    //console.log("Board click");
-    console.log(this.id);
+    // console.log("Board click");
+    // console.log(this.id);
+    count++;
+    moves[this.id] = this.id;
+
+    moves[this.id] = (count % 2) ? 'X' : 'O';
+
+    console.log(moves, count);
+
+
   })
   console.log("standing by");
 });
