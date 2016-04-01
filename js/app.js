e2ba7b8dacb9d5ab5095563abe61ef7f3889e630
D Shaffer <404shaff@gmail.com>
2016-04-01 14:02:13 -0400

    click event

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+var $gameCell = $('.cell');
+
+  $($gameCell).one('click', function() {
+    //console.log("Board click");
+    console.log(this.id);
+  })
   console.log("standing by");
 });
 
