217fb48d7bc8405f1e27cc8045083f720a7c6446
D Shaffer <404shaff@gmail.com>
2016-04-01 15:41:23 -0400

    render X and O

--- js/app.js
+++ js/app.js
@@ -3,16 +3,25 @@ $(document).ready(function() {
 var $gameCell = $('.cell');
 var moves = Array(9).join(".").split(".");
 var count = 0;
+var x = 0;
+var turn = 'X';
 
   $($gameCell).one('click', function() {
     // console.log("Board click");
     // console.log(this.id);
     count++;
-    moves[this.id] = this.id;
+    x = parseInt(this.id);
+    moves[x] = this.id;
+    $(this).html(turn);
 
-    moves[this.id] = (count % 2) ? 'X' : 'O';
+    // moves[x] = (count % 2) ? 'X' : 'O';
+    // turn = moves[x];
+    turn = turn == 'X' ? 'O' : 'X';
 
-    console.log(moves, count);
+    // $gameCell.eq[x].append(moves[x]);
+
+
+    console.log(moves, count, x, turn);
 
 
   })
